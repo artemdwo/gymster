5a971b7962cc4fab4dda91c8880ec792a2e6fa4a
Artem Dvornichenko <artemdwo@gmail.com>
2023-03-07 13:28:08 +0000

    test: :passport_control: Introduce playwright for e2e testing

---
+++ tests/utils.ts
@@ -0,0 +1,23 @@
+import { execSync } from "child_process";
+import detect from "detect-port";
+
+async function startSB() {
+  const port = await detect(54321);
+  if (port !== 54321) {
+    return;
+  }
+  console.warn("Supabase is not running. Starting it now...");
+  execSync("npx supabase start", { stdio: "inherit" });
+}
+
+function reseedDB() {
+  execSync(
+    "PGPASSWORD=postgres psql -U postgres -h 127.0.0.1 -p 64322 -f supabase/clear-db-data.sql",
+    { stdio: "ignore" }
+  );
+}
+
+export async function setupE2eTests() {
+  await startSB();
+  reseedDB();
+}
