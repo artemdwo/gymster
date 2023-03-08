39a1cb8c1b913d9370b11ca24feafba7794d1cbc
Artem Dvornichenko <artemdwo@gmail.com>
2023-03-08 10:57:19 +0000

    feat: :card_file_box: Enable row level security and introduce init policies

--- supabase/migrations/20230307130427_init-ddl.sql
+++ supabase/migrations/20230307130427_init-ddl.sql
@@ -11,4 +11,25 @@ CREATE TABLE
         CONSTRAINT name_regex CHECK (
             username ~* '^[a-zA-Z0-9_]+$'
         )
-    )
+    );
+
+ALTER TABLE user_profiles ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "anyone can see profile" 
+ON "public"."user_profiles" 
+AS PERMISSIVE FOR SELECT 
+TO public 
+USING (true);
+
+CREATE POLICY "only owner can insert" 
+ON "public"."user_profiles" 
+AS PERMISSIVE FOR INSERT 
+TO public 
+WITH CHECK (auth.uid() = user_id);
+
+CREATE POLICY "only owner can uodate" 
+ON "public"."user_profiles" 
+AS PERMISSIVE FOR UPDATE 
+TO public 
+USING (auth.uid() = user_id) 
+WITH CHECK (auth.uid() = user_id);
