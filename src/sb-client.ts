36794123ed8e1391c760a022b0b3e2482e5dda8e
Artem Dvornichenko <artemdwo@gmail.com>
2023-03-08 18:36:30 +0000

    fix: :fire: Don't blindly trust freaking copilot. Always double check whatever this MF generates

--- src/sb-client.ts
+++ src/sb-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
 
-const sbUrl = process.env.VUE_APP_SUPABASE_URL || "";
-const sbKey = process.env.VUE_APP_SUPABASE_KEY || "";
+const sbUrl = (import.meta as any).env.VITE_SUPABASE_API_URL || "";
+const sbKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY || "";
 
 export const sbClient = createClient<Database>(sbUrl, sbKey);
