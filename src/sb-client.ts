36ea3d5f700d5daac7e8d4de1d47bab45424e202
Artem Dvornichenko <artemdwo@gmail.com>
2023-03-08 11:02:31 +0000

    feat: :card_file_box: Generate DB types and use them with SB client

--- src/sb-client.ts
+++ src/sb-client.ts
@@ -1,6 +1,7 @@
-import {createClient} from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
 
-const sbUrl = process.env.VUE_APP_SUPABASE_URL || ''
-const sbKey = process.env.VUE_APP_SUPABASE_KEY || ''
+const sbUrl = process.env.VUE_APP_SUPABASE_URL || "";
+const sbKey = process.env.VUE_APP_SUPABASE_KEY || "";
 
-export const sbClient = createClient(sbUrl, sbKey)
+export const sbClient = createClient<Database>(sbUrl, sbKey);
